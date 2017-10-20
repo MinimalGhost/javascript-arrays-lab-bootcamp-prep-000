ee55458e996af202037d2ee98047c5911212bb8d
Eric Kollegger <eric.kollegger@gmail.com>
2017-10-20 15:11:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -38,6 +38,6 @@ function removeLastKitten() {
 }
 
 function removeFirstKitten() {
-  var newKittens = kittens.slice(0, 1);
+  var newKittens = kittens.slice(1, kittens.length + 1);
   return newKittens
 }
